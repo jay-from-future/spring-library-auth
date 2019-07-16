20e51b028d453c1439bdcf0a0222a2e58d257fe6
Grigorii Liullin <5632433+GrigoriyLyullin@users.noreply.github.com>
2019-07-16 19:47:23 +0300

    Add user, update springframework.security.oauth version due to the bug (#2)

--- src/main/java/ru/otus/springlibrary/SpringLibraryAuth.java
+++ src/main/java/ru/otus/springlibrary/SpringLibraryAuth.java
@@ -1,6 +1,5 @@
 package ru.otus.springlibrary;
 
-import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
